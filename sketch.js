3091ba1ddb4353ab725bece386d5a92e85348672
Raihan1975 <72801070+Raihan1975@users.noreply.github.com>
2021-02-22 21:27:33 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,22 +1,32 @@
 var level1bg;
 var bullet,bulletImg;
 var player1,player1Img;
+var player2Img1,player2Img2,player2Img3;
 var plane,planeImg;
 var gameoverImg;
+var level2bgm;
 var edges;
 var life = 20;
 var bulletGroup;
 var gameState = "level1";
-function preload (){
+var sTime = 50000;
+function preload (){ 
+  /* this is for level1 */
     player1Img = loadImage("images/player1.png");
     level1bg = loadImage("images/level1background.png");
     planeImg = loadImage("images/plane.png");
     bulletImg = loadImage("images/bullet.png");
     gameoverImg = loadImage("images/gameover.jpg");
+ 
+    /* this is for level 2 */
+   player2Img1 = loadAnimation("images/ninja2.png","images/ninja6.png"); 
+ 
+   level2bgm = loadImage("images/background3.jpg") 
+
 }
 function setup() {
   createCanvas(windowWidth,windowHeight);
-  
+/* this is for level 1*/  
   plane=createSprite(width/2,80);
   plane.addImage(planeImg);
   plane.scale=0.3;
@@ -32,6 +42,10 @@ function setup() {
   
   bulletGroup = new Group();
   
+  /* this is for level 2*/
+  player2=createSprite(width/2,height-150);
+  player2.addAnimation("ninja",player2Img1);
+  player2.visible=false;     
   
 }
 
@@ -42,8 +56,11 @@ function draw() {
     background(level1bg);
      textSize(40);
      text("❤:"+life,width-100,200);
-  
-  if(frameCount%10===0){
+     sTime = sTime - 1;
+     if(sTime<0 && life>0){
+       gameState='level2'
+     } 
+     if(frameCount%10===0){
     var planeX =Math.round(random(1,3)); 
    // text(mouseX+","+mouseY,mouseX,mouseY); 
      if(planeX===1){
@@ -53,6 +70,8 @@ function draw() {
      } else {
        plane.x=width/2+450;
      }
+       
+      
     }
     if(life===0){
       gameState="END";
@@ -68,14 +87,23 @@ function draw() {
        spawnBullets();
      }
      
-  for (var i=0; i<bulletGroup.length; i++){
+     for (var i=0; i<bulletGroup.length; i++){
       if(bulletGroup.get(i).isTouching(player1))
      { bulletGroup.get(i).destroy(); life = life - 1; } }
      drawSprites();
     
   }else if(gameState==="END"){
    background(gameoverImg);
-  }
+  } else  if(gameState==="level2"){
+    background(225)
+        player2.visible=true;
+
+        player1.destroy();
+        plane.destroy();
+        bulletGroup.visible=false;
+
+           drawSprites();
+      }
 
 
 }
