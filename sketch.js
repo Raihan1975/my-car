d40c5e62a5c75f7c6aad522a14c8396fec2d9935
Raihan1975 <72801070+Raihan1975@users.noreply.github.com>
2021-02-26 18:43:53 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -2,14 +2,18 @@ var level1bg;
 var bullet,bulletImg;
 var player1,player1Img;
 var player2Img1,player2Img2,player2Img3;
+var ground;
 var plane,planeImg;
+var shriken,shrikenGroup,shrikenImg;
 var gameoverImg;
 var level2bgm;
 var edges;
 var life = 20;
+var superNinja,superNinjaImg;
+var superPower=50;
 var bulletGroup;
 var gameState = "level1";
-var sTime = 50000;
+var sTime = 500;
 function preload (){ 
   /* this is for level1 */
     player1Img = loadImage("images/player1.png");
@@ -20,7 +24,8 @@ function preload (){
  
     /* this is for level 2 */
    player2Img1 = loadAnimation("images/ninja2.png","images/ninja6.png"); 
- 
+   superNinjaImg= loadAnimation("images/ninja7.png","images/ninja14.png");
+   shrikenImg=loadImage("images/shuriken.png");
    level2bgm = loadImage("images/background3.jpg") 
 
 }
@@ -43,14 +48,19 @@ function setup() {
   bulletGroup = new Group();
   
   /* this is for level 2*/
-  player2=createSprite(width/2,height-150);
+  player2=createSprite(width/2-500,height-250);
   player2.addAnimation("ninja",player2Img1);
-  player2.visible=false;     
+  player2.visible=false;   
   
+  player2.addAnimation("power",superNinjaImg);
+
+  ground=createSprite(width/2,height-150,width,10);
+  ground.visible=false;
+  shrikenGroup = new Group(); 
 }
 
 function draw() {
-  
+  superPower=superPower-1;
  if(gameState==="level1"){
     background(60,100,220);
     background(level1bg);
@@ -95,12 +105,44 @@ function draw() {
   }else if(gameState==="END"){
    background(gameoverImg);
   } else  if(gameState==="level2"){
-    background(225)
-        player2.visible=true;
+    background(level2bgm)
+    player1.destroy();
+    plane.destroy();
+    bulletGroup.visible=false;
+   
+    var heart=30;
 
-        player1.destroy();
-        plane.destroy();
-        bulletGroup.visible=false;
+    for (var i=0; i<shrikenGroup.length; i++){
+      if(shrikenGroup.get(i).isTouching(player2))
+     { shrikenGroup.get(i).destroy(); heart = heart - 1; } }
+    
+       
+      
+      
+        player2.visible=true;
+        player2.scale=0.5;
+        player2.velocityY = player2.velocityY+0.8
+        player2.collide(ground);
+        if(keyDown("UP_ARROW")){
+          player2.velocityY=-15;
+        }
+        if(frameCount%80===0){
+        
+        
+          spawnShriken();
+   
+        }
+        if(superPower<0){
+          if(keyDown ("DOWN_ARROW")){
+            player2.changeAnimation("power",superNinjaImg);
+            heart=heart+20;
+            player2.scale=5
+            superPower=superPower+100;
+          }
+        }
+      
+
+        
 
            drawSprites();
       }
@@ -113,10 +155,19 @@ function spawnBullets(){
  bullet.rotation=180;
  bullet.scale=0.5;
  bullet.debug=true;
- bullet.lifetime=height/10;
+ bullet.lifetime=width+100/10;
  bullet.velocityY=10;
   bulletGroup.add(bullet);
   
 }
 
-
+function spawnShriken(){
+  shriken = createSprite(width+100,height-250);
+  shriken.addImage(shrikenImg);
+  shriken.rotation=180;
+  shriken.scale=0.2;
+  shriken.lifetime=height/10;
+  shriken.velocityX=-10;
+  shrikenGroup.add(shriken);
+   
+ }
